165eae1d92a046d2f35c23ab7b4846cd26af3d54
yuuko-amamiya <yuuko-amamiya@protonmail.com>
2020-09-24 20:16:03 +0300

    scripts

--- vue.config.js
+++ vue.config.js
@@ -2,4 +2,21 @@ module.exports = {
   configureWebpack: {
     target: "electron-renderer"
   },
+  pluginOptions: {
+    electronBuilder: {
+      builderOptions: {
+        appId: 'msw.yuuko-amamiya',
+        win: {
+          icon: 'public/windows.ico'
+        },
+        productName: "Minori SoulWorker Launcher",
+        publish: {
+          provider: "github",
+          owner: "MinoriSoulWorker",
+          repo: "launcher",
+          releaseType: "release"
+        }
+      }
+    }
+  }
 };
